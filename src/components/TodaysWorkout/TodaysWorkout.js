4f66b2bd49667e3dd3c0a3bc9d3765d5004881d5
dae-hyun-kim <daehk4@uci.edu>
2020-07-15 20:18:44 +0000

    created Todays Workout component

---
+++ src/components/TodaysWorkout/TodaysWorkout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./TodaysWorkout.module.css"
+
+const TodaysWorkout = () => {
+  return (
+    <div>
+      <h2>hello</h2>
+    </div>
+  )
+}
+
+export default TodaysWorkout;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
-import DateComponent from "../components/Date/DateComponent"
+import DateComponent from "../components/Date/DateComponent";
+import TodaysWorkout from "../components/TodaysWorkout/TodaysWorkout";
 
 const Home = () => {
   return (
     <div>
       <div>
         <DateComponent />
+      </div>
+      <div>
+        <TodaysWorkout/>
       </div>
     </div>
   )
