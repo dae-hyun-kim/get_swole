19e2195c5b00387d94b5d4abb3dfde488ab990ec
dae-hyun-kim <daehk4@uci.edu>
2020-07-31 18:32:57 +0000

    implmenting material ui dash

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
-import DateComponent from "../components/Date/DateComponent";
 import TodaysWorkout from "../components/TodaysWorkout/TodaysWorkout";
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <DateComponent />
-      </div>
       <div className="home-space-around">
         <TodaysWorkout/>
       </div>
